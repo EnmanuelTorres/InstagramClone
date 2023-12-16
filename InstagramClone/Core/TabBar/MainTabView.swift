7fc127d3f022497550906343e4012be756a9c6f3
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2023-12-16 10:38:00 -0500

    Tab Bar Setup

---
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by ENMANUEL TORRES on 15/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Upload")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+            
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
