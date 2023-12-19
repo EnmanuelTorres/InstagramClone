04fcd54c373d4193ad31dadb60274ab903dae0ed
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2023-12-18 20:09:05 -0500

    Upload Post View

--- InstagramClone/Core/TabBar/MainTabView.swift
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -8,33 +8,56 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedIndex) {
             
             FeedView()
+                .onAppear {
+                    selectedIndex = 0
+                }
                 .tabItem {
                     Image(systemName: "house")
                 }
+                .tag(0)
             
             SearchView()
+                .onAppear {
+                    selectedIndex = 1
+                }
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
+                .tag(1)
             
-            Text("Upload")
+            UploadPostView(tabIndex: $selectedIndex)
+                .onAppear {
+                    selectedIndex = 2
+                }
                 .tabItem {
                     Image(systemName: "plus.square")
                 }
+                .tag(2)
             
             Text("Notifications")
+                .onAppear {
+                    selectedIndex = 3
+                }
                 .tabItem {
                     Image(systemName: "heart")
                 }
+                .tag(3)
             
             CurrentUserProfileView(user: User.MOCK_USERS[0])
+                .onAppear {
+                    selectedIndex = 4
+                }
                 .tabItem {
                     Image(systemName: "person")
                 }
+                .tag(4)
             
         }
         .accentColor(.black)
---
+++ InstagramClone/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,73 @@
+//
+//  UploadPostView.swift
+//  InstagramClone
+//
+//  Created by ENMANUEL TORRES on 18/12/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct UploadPostView: View {
+    
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        VStack {
+            // action tool bar
+            HStack {
+                Button {
+                    caption = ""
+                    viewModel .selectedImage = nil
+                    viewModel.postImage = nil
+                    tabIndex = 0
+                } label: {
+                    Text("Cancel")
+                }
+                
+                Spacer()
+                
+                Text("New Post")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Button {
+                print("Upload")
+                } label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                }
+            
+            }
+            .padding(.horizontal)
+            
+            // post image and caption
+            HStack(spacing: 8) {
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption...", text: $caption, axis: .vertical)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .onAppear{
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
+    }
+}
+
+#Preview {
+    UploadPostView(tabIndex: .constant(0))
+}
